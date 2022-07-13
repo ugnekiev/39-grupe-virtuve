b180259beeb91d7ab63a1dac889d60be4fdfeaed
ugne <ugnetomkute@gmail.com>
2022-07-13 16:45:04 +0300

    process

--- js/Kaitlente.js
+++ js/Kaitlente.js
@@ -1,11 +1,21 @@
 class Kaitlente {
     constructor (skyliuKiekis) {
-        this.Size = skyliuKiekis;
-        console.log(this);
+        this.Size = skyliuKiekis
     }
 
     render () {
-        console.log('piesiam turini...');
+        const mainDOM = document.querySelector('main');
+        console.log(mainDOM);
+
+        const HTML = `<div class="kaitlente" style="width: 362px;">
+        <div class="top">
+            <div class="skyle karsta"></div>
+        </div>
+        <div class="bottom">
+            <div class="jungiklis ijungtas"></div>
+        </div>
+    </div>`;
+    mainDOM.innerHTML += HTML;
     }
 }
 
--- js/main.js
+++ js/main.js
@@ -3,14 +3,26 @@ import { Kaitlente } from "./Kaitlente.js";
 // h1 yra virtuve
 // h1 bus dirbtuve
 
-const kaitlenciuKiekis = 5;
+const kaitlenciuKiekis = 3;
 
 //const h1DOM = document.querySelectorAll
-const h1DOM = document.querySelector('h1')
-console.log(h1DOM);
+const titleDOM = document.querySelector('.title')
+titleDOM.innerText = `Dirbtuve (${kaitlenciuKiekis})`;
 
+//console.log(h1DOM.innerText);
+//console.log(h1DOM.textContent);
+document.querySelector('main').textContent = '';
 for (let i = 1; i <= kaitlenciuKiekis; i++) {
     const irenginys = new Kaitlente(i);
     irenginys.render();
-    
+}
+
+// h1 tampa BOLD kai skyliu skaicius lyginis
+if (kaitlenciuKiekis % 2 === 0) {
+    titleDOM.classList.add('bold');
+}
+
+// h1 tampa raudonas kai nors viena skyle ijungta
+if (document.querySelector('.karsta')) {
+    titleDOM.classList.add('red');
 }
